16c1fbe9e44f8801c0a6a2a8460bf0e8715821d5
Ishaan Singh <isingh15@ucsc.edu>
2026-02-17 14:57:15 +0530

    changed routing format to be simpler

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Homepage from "./pages/Homepage";
 import { useMobileLayout } from "./hooks/useMobileLayout";
 import MobileRoutes from "./routes/MobileRoutes";
+import DesktopRoutes from "./routes/DesktopRoutes";
 
 function App() {
   const isMobile = useMobileLayout();
 
   return (
     <>
-      {isMobile ? <MobileRoutes /> : <></> }
+      {isMobile ? <MobileRoutes /> : <DesktopRoutes /> }
     </>
   );
 }
