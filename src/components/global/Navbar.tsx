e9621ee3e6534f5adf615629e6700141ea7535d5
Ishaan Singh <isingh15@ucsc.edu>
2026-02-18 16:51:43 +0530

    changed light mode style

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -11,7 +11,7 @@ import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
 import QuizIcon from "@mui/icons-material/Quiz";
-import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 import Tooltip from "@mui/material/Tooltip";
 
 import { GlobalContext } from "../../contexts/GlobalContextProvider";
@@ -44,10 +44,7 @@ function Navbar() {
     width: "2em",
     height: "2em",
     borderRadius: "50%",
-    color: (theme) =>
-      theme.palette.mode === "dark"
-        ? theme.palette.text.primary
-        : theme.palette.primary.main,
+    color: (theme) => theme.palette.text.primary,
   };
 
   return (
@@ -62,7 +59,7 @@ function Navbar() {
           borderTop: `1px solid ${
             theme.palette.mode === "dark"
               ? "#3a3a3a" // light gray for dark mode
-              : theme.palette.primary.main // blue for light mode
+              : "#000" // blue for light mode
           }`,
         })}
       >
